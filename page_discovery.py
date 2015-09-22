21116025fa674a50779d4fa395fe3f58b13a09bb
zakgier <zakgier@gmail.com>
2015-09-22 11:16:01 -0400

    Finished link completion and link fixing. Now returns a list of all valid links.

--- page_discovery.py
+++ page_discovery.py
@@ -19,14 +19,37 @@ class LinkAggregator():
             endIndex = html.find('"',startFind, end ) #finds the index of the end of the link
             links.append(html[startIndex + displacement:endIndex]) #adds the gathered link to the list
             start = endIndex #moves start up to the end of the last gathered link
-        print(links) #displays all gathered links (currently for testing)
+        return links
 
 
-    def getPageSource(self,url): # just gathers raw HTML from a url and converts to string for processing
+    def getAllLinks(self,url): # takes url, gathers raw html, then runs it through processors to finished links
         with urllib.request.urlopen(url) as response:
-            html = str(response.read())
-            self.parsePage(html)
+            html = str(response.read()) #converts url into string of html
+            links = self.parsePage(html) # parses page into an array of links
+            completeLinks = [] #will hold the completed links
+            i = 0 #used to iterate through and ensure each link is complete
+            while i < len(links):
+                completeLinks.append(self.urlComplete(url,links[i])) #completes the links
+                i = i + 1
+            return completeLinks
 
+    def urlStrip(self, url): # strips a url of one of its end components so it may be slotted into directory references
+        url = url[:url.rfind("/")] # finds farthest back /
+        return url[:url.rfind("/") + 1] #returns stripped end with slash intact
+
+    def urlComplete(self, url, link): #takes broken or incomplete links and formalizes them
+        slices = 0 #used later to fix directory reference
+        if link.find("../") != -1: #if there is directory reference
+            while link.find("../",0,3) != -1: #for each reference
+                link = link[3:] #cut off the ../
+                slices = slices + 1
+            for i in range(slices): #trim the base url to slot in the link
+                url = self.urlStrip(url)
+            return url + link
+        elif link.find("http",0,6) != -1: #if the link is valid, return it
+            return link
+        else: #otherwise, formalize the link
+            return url + link
 
 new = LinkAggregator() #both for functionality testing
-new.getPageSource("http://www.se.rit.edu/~swen-331/projects/fuzzer/")
+print(new.getAllLinks("http://www.se.rit.edu/~swen-331/projects/fuzzer/"))
