7aca37a421d7da26514bba70adaa55e20b9ea280
Douglas <dsw3144@rit.edu>
2015-09-22 21:30:02 -0400

    Update customauth.py
    added some comments

--- customauth.py
+++ customauth.py
@@ -3,9 +3,11 @@ import sys
 
 
 class customauth():
-	site = input("what site?")
+	# commented out since we will be taking the input from args
+	#site = input("what site?")
 	# takes in the argument from the command line and decides which hardcoded authentication to use
 	def getsite(site):
+		#will ignore case of text by making it lower, use the admin and password credentials
 		if site.lower() == 'dvwa':
 	    		url = 'http://127.0.0.1/dvwa/login.php'
 	    		payload = {
@@ -13,6 +15,7 @@ class customauth():
 			'username': 'admin',
     	    		'password': 'password'
 	   		}
+		#will ignore case of text by making it lower and uses sql injection for the admin login
 		if site.lower() == 'bodgeit':
 	    		url = 'http://127.0.0.1:8080/bodgeit/login.jsp'
 	    		payload = {
